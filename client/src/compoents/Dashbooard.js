ff20e734f94d78863ff0c95c1e7070842af1457d
AlbarruSahZihad <zihadkevin7@gmail.com>
2022-11-22 07:28:39 +0700

    upload

---
+++ client/src/compoents/Dashbooard.js
@@ -0,0 +1,20 @@
+// import { useEffect } from 'react'
+// import { useNavigate} from 'react-router-dom'
+
+// import jwt_decode from "jwt-decode"
+
+// function Deshbooard() {
+    
+//     let navigate = useNavigate();
+//     var username;
+//     if (sessionStorage.getItem('token') === null)
+//         username  = '';
+//     }else (
+//         var token = sessionStorage.getItem('token');
+//         var decoded = jwt_decode(token);
+//         username
+//     )
+
+// }
+
+// export default Deshbooard;
